936f37eeaa9b3ffc24230d0170d278b3c15657bc
hanYijin <gksdlwls123@naver.com>
2021-06-17 09:11:38 +0900

    왜오애오오

--- imgadd/main.py
+++ imgadd/main.py
@@ -12,19 +12,20 @@ def home():
 def addimg():
     file1=request.files['file1']
     file2=request.files['file2']
-    file1name=f"/static/{file1.filename}"
-    file2name=f"/static/{file2.filename}"
-    cv2.imwrite(file1name,file1)
-    cv2.imwrite(file2name,file2)
+    file1name=f"static/{file1.filename}"
+    file2name=f"static/{file2.filename}"
+    file1.save(file1name)
+    file2.save(file2name)
 
     img1=cv2.imread(file1name)
     img2=cv2.imread(file2name)
     img3= cv2.add(img1,img2)
-    img4= np.add(img1+img2)
-    cv2addimg="f/static/cv2addimg"
-    npaddimg="f/static/npaddimg"
-    cv2.imwrit(cv2addimg,img3)
-    cv2.imwrite(npaddimg,img4)
-    return render_template("print1.html",cv2add=cv2addimg,npadd=npaddimg)
+
+#    img4= np.add(img1+img2)
+    addimg=f"static/addimg"
+    #npaddimg=f"static/npaddimg"
+    cv2.imwrite(addimg,img3)
+    #cv2.imwrite(npaddimg,img4)
+    return render_template("print1.html",cv2add=addimg)
 
 app.run(host="localhost", port=5000, debug=True)
