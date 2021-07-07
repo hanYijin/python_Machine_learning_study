6ae5c00b3733c30420f385fdf6550c1c5d5e185b
hanYijin <gksdlwls123@naver.com>
2021-07-07 16:44:22 +0900

    sss

---
+++ chatBot0706/0707/vetcor.py
@@ -0,0 +1,17 @@
+import urllib.request
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+import re
+from PIL import Image
+from io import BytesIO
+from nltk.tokenize import RegexpTokenizer
+import nltk
+from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
+from nltk.corpus import stopwords
+from sklearn.metrics.pairwise import cosine_similarity
+
+df = pd.read_csv("../data.csv")
+print('전체 문서의 수 :',len(df))
