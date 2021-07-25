46ba5a489433785b2bbfca22e2cd8447512b4cf3
hanYijin <gksdlwls123@naver.com>
2021-07-26 08:24:40 +0900

    이밎ㅇ

--- 0712img/0712Image/test.py
+++ 0712img/0712Image/test.py
@@ -3,16 +3,8 @@ import cv2
 
 img = cv2.imread('images/d2.png', cv2.IMREAD_COLOR)
 utils.extract_chars(img)
-# green_img = utils.getcolors(img.copy(), utils.GREEN)
-# cv2.imshow('image', green_img)
-# cv2.waitKey(0)
-#
-# blue_img = utils.getcolors(img.copy(), utils.BULE)
-# cv2.imshow('image', blue_img)
-# cv2.waitKey(0)
-# #
-# red_img = utils.getcolors(img.copy(), utils.RED)
-# cv2.imshow('image', red_img)
-# cv2.waitKey(0)
+print(img.shape)
+print(img.size)
+print(img[10,32])
 
 
